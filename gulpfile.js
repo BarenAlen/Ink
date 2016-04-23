88f18fa61598f9923df5b18049a6ab5c0e2477d5
Pavel Lisitsyn <foxfamer@gmail.com>
2016-04-23 18:12:15 +0300

    merge mq and css minification added

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,18 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer'),
 	del = require('del'),
+	combineMq = require('gulp-combine-mq'),
+	concat = require('gulp-concat'),
+	jsmin = require('gulp-jsmin'),
 	browserSync = require('browser-sync').create();
 
 
 gulp.task('clean', function() {
-	return del('public/css');
+	return del(['public/css']);
+});
+
+gulp.task('cleanall', function() {
+	return del(['public/css', 'public/js']);
 });
 
 gulp.task('serve', function() {
@@ -33,9 +40,37 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('less/**/*.*', gulp.series('styles'));
+	gulp.watch(['less/**/*.*', 'js/**/*.*'], gulp.series('styles', 'scripts'));
+});
+
+gulp.task('combineMQ', function() {
+	return gulp.src('public/css/style.css')
+		  .pipe(combineMq({
+		  	beautify: false
+		  }))
+		  .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('scripts', function(){
+	return gulp.src(['bower_components/es5-shim/es5-shim.min.js',
+					 'bower_components/html5shiv/dist/html5shiv.min.js',
+					 'bower_components/svg4everybody/dist/svg4everybody.min.js',
+					 'bower_components/respond/dest/respond.min.js',
+					 'bower_components/morpheus/morpheus.min.js',
+					 'js/swipe.js',
+					 'js/action.js'
+					 ])
+		  .pipe(sourcemaps.init())
+		  .pipe(concat('main.js'))
+		  .pipe(sourcemaps.write('.'))
+		  .pipe(debug({title: 'concatenate'}))
+		  .pipe(jsmin())
+		  .pipe(debug({title: 'JS minification'}))
+		  .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('build', gulp.series('clean', gulp.parallel('styles')));
 
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
+
+gulp.task('production', gulp.series('cleanall', 'scripts', gulp.parallel('styles'), 'combineMQ'));
